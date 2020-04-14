fd41389e341279f7eef6fff5184963572a78068e
ANURAG GUPTA <anuraggupta29@outlook.com>
2020-04-14 07:51:29 +0530

    updated form

--- static/js/form.js
+++ static/js/form.js
@@ -1,4 +1,18 @@
-//SEND SELECTED STATE, CITY TO THE NEXT PAGE
-document.querySelector('.submitButton').addEventListener('click',function(){
-    
-});
+
+
+/***************NAVBAR*********************/
+var mediaWidth = window.matchMedia("(min-width: 640px)")
+
+function openNav() {
+    if (mediaWidth.matches) {
+        document.querySelector(".sidenav").style.width = "30vw";
+      } else {
+        document.querySelector(".sidenav").style.width = "60vw";
+      }
+      document.querySelector(".navSlideMenu").style.display = "None";
+  }
+  
+  function closeNav() {
+    document.querySelector(".sidenav").style.width = "0";
+    document.querySelector(".navSlideMenu").style.display = "flex";
+  }
